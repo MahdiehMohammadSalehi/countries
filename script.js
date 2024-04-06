f6ee9ad2faf5b1aff2bc3d6759ccfedde7271374
unknown <msalehi.mahdieh@gmail.com>
2024-04-06 23:04:34 +0330

    baseProject

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+const container = document.querySelector(".container");
+
+const countries = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    let data = JSON.parse(this.responseText);
+    data = data[0];
+    const lang = Object.values(data.languages);
+    const money = Object.values(data.currencies);
+
+    console.log(data);
+    const html = ` <div class="countries">
+  <img class="flag" src=${data.flags.png} alt="" />
+  <ul>
+    <li class="name"> ${data.altSpellings[1]}</li>
+    <li class="region">${data.region}</li>
+    <li class="population">🧑‍🤝‍🧑${(+data.population / 1000000).toFixed(
+      1
+    )}M people</li>
+    <li class="region">🗣️ ${lang}</li>
+    <li class="corrency">💰 ${money[0].name}</li>
+  </ul>
+</div>`;
+    container.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+countries("iran");
+countries("usa");
+countries("korea");
